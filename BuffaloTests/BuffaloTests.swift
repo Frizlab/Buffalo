3b4bb32b9513a9feaa78b298cc5f19ab10cea35b
Frizlab <fload@me.com>
2016-11-20 11:27:50 +0100

    Initial Commit

---
+++ BuffaloTests/BuffaloTests.swift
@@ -0,0 +1,36 @@
+//
+//  BuffaloTests.swift
+//  BuffaloTests
+//
+//  Created by François Lamboley on 11/20/16.
+//  Copyright © 2016 Frizlab. All rights reserved.
+//
+
+import XCTest
+@testable import Buffalo
+
+class BuffaloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
